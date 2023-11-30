6a6e0b05967cab413707d51109d809420226bcaa
Juan Polindara <juan@polindara.com>
2023-11-29 23:22:14 -0500

    Este commmit solo le falta el css y el no hay tareas pendientes

--- TO_DO.js
+++ TO_DO.js
@@ -6,7 +6,22 @@ $btnAdd.addEventListener("click", (e) => {
   e.preventDefault();
   // Esto funciona para agregar las tareas al todo list
   const $li = document.createElement("li");
+
   $li.textContent = $inpAdd.value;
+  $li.appendChild(btndelete());
   $listUl.appendChild($li);
   $inpAdd.value = "";
 });
+
+function btndelete() {
+  const $btndel = document.createElement("button");
+  $btndel.textContent = "X";
+
+  $btndel.addEventListener("click", (e) => {
+    e.preventDefault();
+    const item = e.target.parentElement;
+    $listUl.removeChild(item);
+  });
+
+  return $btndel;
+}
