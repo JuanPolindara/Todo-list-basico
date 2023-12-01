2c3940c7485b8c13ca44a1d4d757472d0bb656e3
Juan Polindara <juan@polindara.com>
2023-11-30 23:51:01 -0500

    TODO list basico sin backend terminado

--- TO_DO.js
+++ TO_DO.js
@@ -5,16 +5,23 @@ const $listUl = document.querySelector(".list-ul");
 $btnAdd.addEventListener("click", (e) => {
   e.preventDefault();
   // Esto funciona para agregar las tareas al todo list
-  const $li = document.createElement("li");
-
-  $li.textContent = $inpAdd.value;
-  $li.appendChild(btndelete());
-  $listUl.appendChild($li);
-  $inpAdd.value = "";
+  if ($inpAdd.value === "") {
+    alert("No se aceptan tareas vacias 😜");
+  } else {
+    const $li = document.createElement("li");
+    const $p = document.createElement("p");
+    $p.textContent = $inpAdd.value;
+    $li.appendChild($p);
+    $li.appendChild(btnmarcar());
+    $li.appendChild(btndelete());
+    $listUl.appendChild($li);
+    $inpAdd.value = "";
+  }
 });
 
 function btndelete() {
   const $btndel = document.createElement("button");
+  $btndel.classList.add("btndel");
   $btndel.textContent = "X";
 
   $btndel.addEventListener("click", (e) => {
@@ -24,4 +31,20 @@ function btndelete() {
   });
 
   return $btndel;
+}
+
+function btnmarcar() {
+  const $btnmark = document.createElement("input");
+  $btnmark.type = "checkbox";
+
+  $btnmark.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    if ($btnmark.checked) {
+      item.classList.add("checked");
+    } else {
+      item.classList.remove("checked");
+    }
+  });
+
+  return $btnmark;
 }
